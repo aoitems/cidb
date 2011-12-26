6a0b5bfc1a8d6694fde9e4797b1c662db449b242
Savagedlight <Savagedlight@localhost>
2011-12-26 07:34:16 +0100

    Added checks for whether or not we're inside CMS to more include files.

--- v2.0.php
+++ v2.0.php
@@ -1,4 +1,10 @@
 <?php
+if (!INSIDE_CMS)
+{
+	header("location: /");
+	return;
+}
+
 // Output type
 $output = strip_tags(basename(strtolower($_GET['output'])));
 if ($output!="xml" && $output!="json")
